64d7dcc8a1431e1e53f8c1d8bdf9fad2eb22f986
MRXPRO200330020179Y <mrxpro2003@gmail.com>
2022-08-23 17:32:18 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+});
+});
+  // Or with jQuery
+$(document).ready(function(){
+  $('.dropdown-trigger').dropdown();
+});
+
+
+$(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+});
+  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  // var collapsibleElem = document.querySelector('.collapsible');
+  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
